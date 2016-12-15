d357cddc68645214410de268fd73b9fc8a292e5b
derekawilson72 <derekawilson72@gmail.com>
2016-12-15 15:03:48 -0500

    Update remoteProcessAccess.py

--- remoteProcessAccess.py
+++ remoteProcessAccess.py
@@ -2,7 +2,10 @@
 ##Python program to run a process in the background within its own IPython kernel.
 ##Author: Derek Wilson
 ##Modified: 11 Dec 2016
-####This python module will start a Python process in the background while allowing users to monitor the process status and intecede (if necessary) in any of the calculations or procedure.  Opening the process within the Ipython console allows users to access resources otherwise hidden from view and detached from user's control.  
+####This python module will start a Python process in the background while allowing 
+###users to monitor the process status and intecede (if necessary) in any of the 
+###calculations or procedure.  Opening the process within the Ipython console allows 
+###users to access resources otherwise hidden from view and detached from user's control.  
 
 from multiprocessing import Process, Value, Array
 from threading import Thread
@@ -11,7 +14,9 @@ from threading import Thread
 class remoteProc():
     """The remoteProc class contains the shared Python variables as well
     as its own parameters and the "complicated" function requiring
-    running in the background as its own process.  The main process can access the multiprocessing variables of num, arr and keepLooping.  Users can control the loop running in a thread by setting the value to 0.  
+    running in the background as its own process.  The main process can access 
+    the multiprocessing variables of num, arr and keepLooping.  Users can control 
+    the loop running in a thread by setting the value to 0.  
 
     """
     t1=None #held for thread
@@ -27,7 +32,8 @@ class remoteProc():
                  arr= Array('f', range(10)),
                  keepLooping=Value('i', 1)):
         """
-        Instantiate the class setting the default variables.  Set  keepLooping to 0 to kill the loops.
+        Instantiate the class setting the default variables.  
+        Set  keepLooping to 0 to kill the loops.
         """
         self.num=num
         self.arr=arr
@@ -35,7 +41,9 @@ class remoteProc():
     
     def doComplicatedStuff(self):
         """
-        The arbitrary function or procedure to run in the background.  import anything here and control the loop by setting the keepLooping value to 0.  Also watch the num variable increment by one each time.
+        The arbitrary function or procedure to run in the background.  
+        import anything here and control the loop by setting the keepLooping 
+        value to 0.  Also watch the num variable increment by one each time.
         """
         import time
         while self.keepLooping.value>0:
@@ -46,7 +54,8 @@ class remoteProc():
 
     def startThread(self):
         """
-        Run the function as a thread to allow users to interact with the class parameters independantly in Ipython.
+        Run the function as a thread to allow users to interact with the class
+        parameters independantly in Ipython.
         """
         t1=Thread(target=self.doComplicatedStuff, args=())
         t1.start()
@@ -56,7 +65,8 @@ class remoteProc():
 
 def launchIPythonProcess(num,arr,keepLooping):
     """
-    Call the remoteProc function and start the thread.  Finally call Ipython to run the class functions in a thread in the background
+    Call the remoteProc function and start the thread.  Finally call Ipython to run the 
+    class functions in a thread in the background
     """
     import IPython
     rP1=remoteProc(num,arr,keepLooping)
@@ -67,7 +77,9 @@ def launchIPythonProcess(num,arr,keepLooping):
 
 def main():
     """
-    Main test script to instantiate the shared variables, call the remote process and run the process in the background.  Users can then access this remote process by opening its kernel in Ipython
+    Main test script to instantiate the shared variables, call the remote process and 
+    run the process in the background.  Users can then access this remote process by
+    opening its kernel in Ipython
     """
     num = Value('d', 0.0)
     arr = Array('f', range(10))
